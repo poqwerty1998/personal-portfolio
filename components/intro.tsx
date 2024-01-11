45e1b25a5c07cddacf88d4c2956e7130e2da3987
poqwerty1998 <31867171+poqwerty1998@users.noreply.github.com>
2024-01-11 17:08:25 -0500

    about me section temporarily finished

--- components/intro.tsx
+++ components/intro.tsx
@@ -50,20 +50,18 @@ export default function Intro() {
           </motion.div>
         </div>
       </div>
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hey! I'm Peter Xiao.</span> I'm a{" "}
+        <span className="font-bold">Hey! I'm Peter.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">3 years</span> of experience. My passion is
         in building <span className="italic">front-end applications</span>. My
-        current focus is{" "}
-        <span className="underline">
-          exploring the world of web development with React.
-        </span>
-      </motion.p>
+        current focus is exploring the world of web development with{" "}
+        <span className="underline">React.</span>
+      </motion.h1>
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
@@ -76,29 +74,39 @@ export default function Intro() {
         <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none
-          hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
         >
-          Contact me here! <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
+          Contact me here!{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
-        <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none
-          hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none
+        hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
           href="/Peter Xiao Resume.pdf"
-          download>
-          Download Resume <HiDownload className="opacity-60 group-hover:transition-y-1 transition"/>
+          download
+        >
+          Download Resume{" "}
+          <HiDownload className="opacity-60 group-hover:transition-y-1 transition" />
         </a>
 
-        <a className="bg-white px-4 py-3 text-gray-700 flex items-center gap-2 rounded-full focus-scale[1.15]
-        hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10">
+        <a
+          className="bg-white px-4 py-3 text-gray-700 flex items-center gap-2 rounded-full focus-scale[1.15]
+        hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/peter-xiao-156978149/"
+          target="_blank"
+        >
           <BsLinkedin />
         </a>
 
-        <a className="bg-white px-4 py-3 text-gray-700 flex items-center gap-2 text-[1.35rem] focus-scale-[1.15] rounded-full
+        <a
+          className="bg-white px-4 py-3 text-gray-700 flex items-center gap-2 text-[1.35rem] focus-scale-[1.15] rounded-full
         hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
-        href="">
+          href="https://github.com/poqwerty1998"
+          target="_blank"
+        >
           <FaGithubSquare />
         </a>
-
       </motion.div>
     </section>
   );
