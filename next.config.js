6840d358fd91feacc90f09d4ebfdb40bab565751
poqwerty1998 <31867171+poqwerty1998@users.noreply.github.com>
2024-01-11 16:32:10 -0500

    Intro section close to completion

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  //   images: {
+  //     remotePatters: [
+  //       {
+  //         protocol: "https",
+  //         hostname: "",
+  //       },
+  //     ],
+  //   },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
