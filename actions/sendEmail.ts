3186e9c63ffcefd68390495d87d90f8627436a5a
poqwerty1998 <31867171+poqwerty1998@users.noreply.github.com>
2024-01-13 11:39:38 -0500

    email functionality completed
    need to style emails sent

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,25 +1,9 @@
 "use server";
 
 import { Resend } from "resend";
-import { validateString } from "@/lib/utils";
+import { validateString, getErrorMessage } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-const getErrorMessage = (error: unknown) => {
-  let message: string;
-  if (error instanceof Error) {
-    return {
-      error: error.message,
-    };
-  } else if (error && typeof error === "object" && "message" in error) {
-    return {
-      error: error.message,
-    };
-  }
-  return {
-    message
-  };
-};
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
